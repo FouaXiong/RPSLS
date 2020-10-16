18a56515f27c976af1656828d619f41962ae4189
Foua Xiong <foua.forte@gmail.com>
2020-10-16 13:56:29 -0500

    Initial Commit

---
+++ RPSLS/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+   public class Human : Player
+    {
+        // Member Variable
+        public string name;
+
+
+        // Constructor
+        public Human(string name)
+        {
+            this.name = name;
+        }
+        public override void ChooseGestures()
+        {
+
+        }
+
+
+    }
+}
---
+++ RPSLS/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public abstract class Player
+    {
+        // Member Variable
+        public string gesture;
+        int score;
+        public abstract void chooseGestures();
+        public List<string> Gestures;
+        
+        // Constructor
+        public Player()
+        {
+            Gestures = new List<string>()
+            {
+                "rock", "paper", "scissor", "lizard", "Spock"
+            };
+        }
+
+        // Member Method
+            // What a Player can do
+        public abstract void ChooseGesture()
+        {
+            Console.WriteLine("has chosen" + Gestures);
+        }
+    
+    }
+
+
+
+}
